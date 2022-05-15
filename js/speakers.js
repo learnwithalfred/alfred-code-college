ecc59e08f150a2bdde296bbe365ae6a9ec2a7f75
Alfred Boateng <programmingwithalfred@gmail.com>
2022-05-15 20:35:18 +0000

    Personalize content of website

--- js/speakers.js
+++ js/speakers.js
@@ -5,14 +5,14 @@ const data = [
     about: 'Curabitur in libero ut massa volutpat convallis. Morbi odio odio',
     message:
       'Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis.',
-    img: 'img/speaker_03.png',
+    img: 'img/speakers/1.png',
   },
   {
     id: 'd28c9e17-7dd9-4bc0-8775-0e03fb93665d',
     name: 'Devan Ropkins',
     about: 'Fusce posuere felis sed lacus. Morbi sem mauris',
     message: 'Fusce consequat. Nulla nisl. Nunc nisl.',
-    img: 'img/speaker_03.png',
+    img: 'img/speakers/2.png',
   },
   {
     id: 'dc1a61d9-1a70-4ad9-ad62-13a123865cbf',
@@ -21,7 +21,7 @@ const data = [
       'Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula',
     message:
       'Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.',
-    img: 'img/speaker_03.png',
+    img: 'img/speakers/3.png',
   },
   {
     id: 'bfa8df75-7556-43d0-a147-7c3e6fb7777f',
@@ -30,7 +30,7 @@ const data = [
       'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis.',
     message:
       'Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus ',
-    img: 'img/speaker_03.png',
+    img: 'img/speakers/4.png',
   },
   {
     id: '62c8e787-bcc3-40c3-b80f-9f0a90402a56',
@@ -38,30 +38,14 @@ const data = [
     about:
       'In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.',
     message: 'Quisque id justo sit amet sapien dignissim vestibulum.',
-    img: 'img/speaker_03.png',
+    img: 'img/speakers/5.png',
   },
   {
     id: '2219ca0c-403d-42ac-ab30-00674c16b25c',
     name: 'Ingrid Almon',
     about: 'Phasellus in felis. Donec semper sapien a libero. Nam dui.',
     message: 'Cum sociis natoque penatibus et magnis dis parturient montes',
-    img: 'img/speaker_03.png',
-  },
-  {
-    id: '9693c1ab-a6a8-44c9-9e78-fccee1700533',
-    name: 'Doralynne Drinkhall',
-    about:
-      'In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.',
-    message: 'Fusce consequat. Nulla nisl. Nunc nisl.',
-    img: 'img/speaker_03.png',
-  },
-  {
-    id: 'eeba234e-7f5a-41ef-b88d-4615c37feecd',
-    name: 'Felice Assandri',
-    about: 'Cum sociis natoque penatibus et magnis dis parturient montes',
-    message:
-      'Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo.',
-    img: 'img/speaker_03.png',
+    img: 'img/speakers/6.png',
   },
 ];
 
@@ -72,6 +56,8 @@ for (let i = 0; i < data.length; i += 1) {
 
   dynamic.innerHTML = ` 
     <div class="col">
+                <div class="speakers--content">
+
       <div class="speakers__card">
       <div class="speakers__img--container">
           <img
@@ -82,7 +68,7 @@ for (let i = 0; i < data.length; i += 1) {
           />
         </div>
         <div class="speakers__card--info">
-          <h5 class'speakers__card--header'>${data[i].name}</h5>
+          <h5 class='speakers__card--header'>${data[i].name}</h5>
           <p class="speakers__card--info__about">
             ${data[i].about}
           </p>
@@ -91,6 +77,8 @@ for (let i = 0; i < data.length; i += 1) {
             ${data[i].message}
           </p>
         </div>
+       </div>
+
       </div>
     </div>
     
